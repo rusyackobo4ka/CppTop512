499cbb97b0617ad3f9a7745699062db4c792a2f1
Ruslan Magovedov <rysikmagom10@gmail.com>
2025-12-13 16:00:17 +0300

    [REF] : [GIT] Git Init Commit

---
+++ Main/Main.cpp
@@ -0,0 +1,44 @@
+﻿// Текст комментариев
+#include <iostream> // Подключение библиотеки ввода/вывода
+#include <string>
+#include <stdlib.h>
+#include <cstdlib>
+#include <math.h>
+
+using namespace std;
+
+#define ARRAY_SIZE 5
+
+int main()
+{
+    int firstArr[ARRAY_SIZE] = { 3365, 2, 76, 4, 5 };
+
+    int secondArr[ARRAY_SIZE];
+
+    int* firstStartPtr = firstArr;
+    int* secondEndPtr = secondArr + ARRAY_SIZE - 1;
+
+
+    for (int i = 0; i < ARRAY_SIZE; ++i)
+    {
+        *secondEndPtr = *firstStartPtr;
+        firstStartPtr++;
+        secondEndPtr--;
+    }
+
+    for (int i = 0; i < ARRAY_SIZE; ++i)
+    {
+        cout << firstArr[i] << " ";
+    }
+
+    cout << endl << "-=========================-" << endl;
+
+    for (int i = 0; i < ARRAY_SIZE; ++i)
+    {
+        cout << secondArr[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
---
+++ Main/homeword 2.2.cpp
@@ -0,0 +1,45 @@
+﻿// Текст комментариев
+#include <iostream> // Подключение библиотеки ввода/вывода
+#include <string>
+#include <stdlib.h>
+#include <cstdlib>
+#include <math.h>
+
+using namespace std;
+
+#define ARRAY_SIZE 5
+
+int main()
+{
+    int Arr[ARRAY_SIZE] = { 3365, 2, 76, 4, 5 };
+
+    int* startPtr = Arr;
+
+    int* endPtr = Arr + ARRAY_SIZE - 1;
+
+    for (int i = 0; i < ARRAY_SIZE; ++i)
+    {
+        cout << Arr[i] << " ";
+    }
+
+    while (startPtr < endPtr)
+    {
+        int temp = *startPtr;
+        *startPtr = *endPtr;
+        *endPtr = temp;
+
+        startPtr++;
+        endPtr--;
+    }
+
+    cout << endl << "-=========================-" << endl;
+
+    for (int i = 0; i < ARRAY_SIZE; ++i)
+    {
+        cout << Arr[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
