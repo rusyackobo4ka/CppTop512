23c4cc59f0559f750e169615236e8fc6fd192e38
Ruslan Magovedov <rysikmagom10@gmail.com>
2025-12-13 17:47:07 +0300

    [ADD]:[Homework] Homework 12.09 + 13.09 + 20.09 + 25.09

---
+++ Main/FWW/homework 2.1.cpp
@@ -0,0 +1,46 @@
+﻿// Текст комментариев
+#include <iostream> // Подключение библиотеки ввода/вывода
+#include <string>
+#include <stdlib.h>
+#include <cstdlib>
+#include <math.h>
+
+using namespace std;
+
+#define ARRAY_SIZE 5
+
+int main()
+{
+    int firstArr[ARRAY_SIZE] = { 3365, 2, 76, 4, 5 };
+
+    int secondArr[ARRAY_SIZE];
+
+    int* firstPtr = firstArr;
+    int* secondPtr = secondArr;
+
+
+    for (int i = 0; i < ARRAY_SIZE; ++i)
+    {
+        *secondPtr = *firstPtr;
+        firstPtr++;
+        secondPtr++;
+    }
+
+
+
+    for (int i = 0; i < ARRAY_SIZE; ++i)
+    {
+        cout << firstArr[i] << " ";
+    }
+
+    cout << endl << "-=========================-" << endl;
+
+    for (int i = 0; i < ARRAY_SIZE; ++i)
+    {
+        cout << secondArr[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
