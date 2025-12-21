98ccb25a6f195795611c71a7d7fac81d4d2f65a4
Ruslan Magovedov <rysikmagom10@gmail.com>
2025-12-21 15:47:49 +0300

    [ADD]:[HOMEWORK] homework 27.12 + 20.11

---
+++ Main/FHW/27.12 + 20.11/homework 1.1.cpp
@@ -0,0 +1,73 @@
+﻿// Текст комментариев
+#include <iostream> // Подключение библиотеки ввода/вывода
+#include <string>
+#include <stdlib.h>
+#include <cstdlib>
+#include <math.h>
+
+using namespace std;
+
+#define ARRAY_SIZE 4
+
+int main()
+{
+    int arr[ARRAY_SIZE][ARRAY_SIZE] = {
+                                       {48,65,78,45},
+                                       {54,44,57,67},
+                                       {32,45,87,37},
+                                        {22,54,77,88}
+    };
+
+
+    int num = { 0 };
+
+    for (int i = 0; i < ARRAY_SIZE; ++i)
+    {
+        for (int j = 0; j < ARRAY_SIZE; ++j)
+        {
+            cout << arr[i][j] << ' ';
+        }
+        cout << endl;
+    }
+
+    cout << "Enter num: ";
+    cin >> num;
+    cout << endl;
+
+    int newArr[ARRAY_SIZE][ARRAY_SIZE + 1];
+
+    for (int i = 0; i < ARRAY_SIZE; ++i)
+    {
+        for (int j = 0; j < ARRAY_SIZE; ++j)
+        {
+            newArr[i][j] = arr[i][j];
+        }
+    }
+
+
+    
+
+    for (int i = num + 1; i < ARRAY_SIZE; ++i)
+    {
+        for (int j = 0; j < ARRAY_SIZE; ++j)
+        {
+            newArr[j][i + 1] = arr[j][i];
+        }
+        
+    }
+
+    for (int j = 0; j < ARRAY_SIZE; ++j)
+    {
+        newArr[j][num + 1] = 0;
+    }
+
+    for (int i = 0; i < ARRAY_SIZE; ++i)
+    {
+        for (int j = 0; j < ARRAY_SIZE + 1; ++j)
+        {
+            cout << newArr[i][j] << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
---
+++ Main/FHW/27.12 + 20.11/homework 1.3.cpp
@@ -0,0 +1,221 @@
+﻿// Текст комментариев
+#include <iostream> // Подключение библиотеки ввода/вывода
+#include <string>
+#include <stdlib.h>
+#include <cstdlib>
+#include <math.h>
+
+using namespace std;
+
+#define ARRAY_Y_SIZE 2
+#define ARRAY_X_SIZE 6
+
+void func(int num, char turn, int arr[ARRAY_Y_SIZE][ARRAY_X_SIZE])
+{
+    int tmpArr[ARRAY_Y_SIZE][ARRAY_X_SIZE];
+
+    for (int y = 0; y < ARRAY_Y_SIZE; ++y)
+    {
+        for (int x = 0; x < ARRAY_X_SIZE; ++x)
+        {
+            tmpArr[y][x] = arr[y][x];
+        }
+    }
+
+    if (turn == 'L')
+    {
+        if (num < 0)
+        {
+            num = ARRAY_X_SIZE + num;
+        }
+
+        while (num > ARRAY_X_SIZE)
+        {
+            num -= ARRAY_X_SIZE;
+        }
+
+        int newMove = { 0 };
+
+        for (int y = 0; y < ARRAY_Y_SIZE; ++y)
+        {
+            for (int x = 0; x < ARRAY_X_SIZE; ++x)
+            {
+                if (x + num < ARRAY_X_SIZE)
+                {
+                    arr[y][x] = tmpArr[y][x + num];
+                }
+                else
+                {
+                    newMove = ARRAY_X_SIZE - num;
+                    arr[y][x] = tmpArr[y][x - newMove];
+                }
+
+
+            }
+        }
+    }
+
+    else if (turn == 'R')
+    {
+        if (num < 0)
+        {
+            num = ARRAY_X_SIZE + num;
+        }
+
+        while (num > ARRAY_X_SIZE)
+        {
+            num -= ARRAY_X_SIZE;
+        }
+
+        int newMove = { 0 };
+
+        for (int y = 0; y < ARRAY_Y_SIZE; ++y)
+        {
+            for (int x = 0; x < ARRAY_X_SIZE; ++x)
+            {
+                if (x - num < 0)
+                {
+                    newMove = ARRAY_X_SIZE - num;
+                    arr[y][x] = tmpArr[y][x + newMove];
+                }
+                else
+                {
+                    arr[y][x] = tmpArr[y][x - num];
+                }
+
+            }
+        }
+    }
+
+    else if (turn == 'F')
+    {
+        if (num < 0)
+        {
+            num = ARRAY_Y_SIZE + num;
+        }
+
+        while (num > ARRAY_Y_SIZE)
+        {
+            num -= ARRAY_Y_SIZE;
+        }
+
+        int newMove = { 0 };
+
+        for (int x = 0; x < ARRAY_X_SIZE; ++x)
+        {
+            for (int y = 0; y < ARRAY_Y_SIZE; ++y)
+            {
+                if (y - num < 0)
+                {
+                    newMove = ARRAY_Y_SIZE - num;
+                    arr[y][x] = tmpArr[y + newMove][x];
+                }
+                else
+                {
+                    arr[y][x] = tmpArr[y - num][x];
+                }
+
+            }
+        }
+    }
+
+    else if (turn == 'B')
+    {
+        for (int x = 0; x < ARRAY_X_SIZE; ++x)
+        {
+            if (num < 0)
+            {
+                num = ARRAY_Y_SIZE + num;
+            }
+
+            while (num > ARRAY_Y_SIZE)
+            {
+                num -= ARRAY_Y_SIZE;
+            }
+
+            int newMove = { 0 };
+
+            for (int x = 0; x < ARRAY_X_SIZE; ++x)
+            {
+                for (int y = 0; y < ARRAY_Y_SIZE; ++y)
+                {
+                    if (y + num < ARRAY_X_SIZE)
+                    {
+                        arr[y][x] = tmpArr[y + num][x];
+                    }
+                    else
+                    {
+                        newMove = ARRAY_Y_SIZE - num;
+                        arr[y][x] = tmpArr[y - newMove][x];
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+int main()
+
+{
+    int arr[ARRAY_Y_SIZE][ARRAY_X_SIZE] =
+    {
+        {1, 2, 0, 4, 5, 3},
+        {4, 5, 3, 9, 0, 1}
+    };
+
+    for (int i = 0; i < ARRAY_Y_SIZE; ++i)
+    {
+        for (int j = 0; j < ARRAY_X_SIZE; ++j)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    int num = { 0 };
+    char turn = { 'E' };
+    char choice = { 'N' };
+
+    while (true)
+    {
+        cout << "Enter num(0 to do nothing): ";
+        
+        cin >> num;
+
+        if (num == 0)
+        {
+            break;
+        }
+
+        cout << endl;
+
+        cout << "Enter turn(L, R, F, B): ";
+        
+        cin >> turn;
+
+        func(num, turn, arr);
+
+        cout << endl << "something else?(Y or N) ";
+        cin >> choice;
+
+        if (choice == 'N')
+        {
+            break;
+        }
+    }
+
+    
+
+    cout << endl << endl << endl;
+
+    for (int i = 0; i < ARRAY_Y_SIZE; ++i)
+    {
+        for (int j = 0; j < ARRAY_X_SIZE; ++j)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
