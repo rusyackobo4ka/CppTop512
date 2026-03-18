cf0112269a245bdfc8067adc3993195b56b810b6
Ruslan Magovedov <rysikmagom10@gmail.com>
2026-03-18 18:56:56 +0300

    retry

---
+++ Main/FHW/FCW/ClassWork 18.03 sec.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <list>
+
+
+std::list<int> func(int num)
+{
+    std::list<int> result;
+    int a = 0, b = 1;
+
+    while (true) 
+    {
+        int tmp = a + b;
+        if (tmp > num)
+        {
+            break;
+        }
+
+        result.push_back(tmp);
+        a = b;
+        b = tmp;
+    }
+
+    return result;
+}
+
+int main()
+{
+    std::list<int> test = func(15);
+
+    for (auto iter = test.begin(); iter != test.end(); ++iter)
+    {
+        std::cout << *iter << " ";
+    }
+    
+}
